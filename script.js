fd881bbd11efd29aa19d8e197225d2475bbf8332
Larraine Gonzales <larrainegonzales@hotmail.com>
2020-08-20 18:57:52 -0500

    updated javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+function setNewLogo()
+{
+    document.getElementById("img1").src="https://user-images.githubusercontent.com/68169895/90835696-e2ab1200-e312-11ea-92a9-b80be71ff093.png";
+}
+function setOldImage()
+{
+    document.getElementById("img1").src="https://user-images.githubusercontent.com/68169895/90836612-754cb080-e315-11ea-9184-b4273ed51a1f.png";
+}
